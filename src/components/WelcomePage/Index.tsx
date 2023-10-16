01a41a3518923b36794ccfd9b36baa7134fc01eb
Jacob Hawkins <jacob.hawkins010@gmail.com>
2023-10-16 12:13:19 -0400

    style changes

--- src/components/WelcomePage/Index.tsx
+++ src/components/WelcomePage/Index.tsx
@@ -4,12 +4,23 @@ import uw211Logo from './images/unitedway211.png';
 function WelcomePage(props: { name: string }) {
   return (
     <div className="flex items-center justify-center w-screen h-screen">
-      <div className="flex items-center justify-center flex-wrap flex-intial w-11/12 h-11/12">
-        <div className="flex-1 flex items-center justify-center flex-col">
-          <h1>Welcome {props.name}!</h1>
-          <button className="bg-[#1e57a1] border-none p-2 rounded-sm">
-            Continue!
-          </button>
+      <div className="flex items-center justify-center flex-wrap w-5/6 h-5/6">
+        <div className="flex-1 w-1/3 h-2/3 bg-slate-200 rounded-2xl">
+          <div className="w-full h-full flex justify-center flex-col gap-5 p-8">
+            <div>
+              <h2 className="text-[#df593e] text-xl font-semibold">
+                211 Knoxville
+              </h2>
+              <h1 className="text-3xl">
+                Welcome
+                <span className="font-bold"> {props.name}</span>!
+              </h1>
+              <p className=""></p>
+            </div>
+            <button className="bg-[#1e57a1] border-none p-2 rounded-sm text-white w-48">
+              Get Started!
+            </button>
+          </div>
         </div>
         <div className="flex-1 flex items-center justify-center">
           <img
