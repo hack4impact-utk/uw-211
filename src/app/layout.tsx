0a9feb7620a8d0f5b8d80c890c08a5b1af32de02
Dylan Toth <dylant1@yahoo.com>
2023-10-18 11:41:35 -0400

    Add basic navbar with minimal styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import Navbar from '../components/navbar';
 
 export const metadata = {
   title: 'Next.js',
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
