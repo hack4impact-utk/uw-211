9a0d17a54f4f48dff635c2472a60130e64a863ca
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2023-10-23 20:46:35 -0400

    basic table with clickable email links

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,33 @@
+import { AdminDashboardTable } from '@/components/AdminDashboardTable';
+import { Nonprofit } from '@/components/AdminDashboardTable';
+
+const Data: Nonprofit[] = [
+  {
+    name: 'test1',
+    status: 'close to deadline',
+    email: 'ffff',
+  },
+  {
+    name: 'test2',
+    status: 'email sent recently',
+    email: 'jjjj',
+  },
+  {
+    name: 'test3',
+    status: 'expired',
+    email: 'hhhh',
+  },
+  {
+    name: 'test4',
+    status: 'up-to-date',
+    email: 'gggg',
+  },
+];
+
 export default function Dashboard() {
-  return <h1 className="underline">Welcome to the Admin Dashboard!</h1>;
+  return (
+    <div className="p-2">
+      <AdminDashboardTable data={Data} />
+    </div>
+  );
 }
---
+++ src/components/AdminDashboardTable/index.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+
+export type Nonprofit = {
+  name: string;
+  status:
+    | 'expired'
+    | 'email sent recently'
+    | 'up-to-date'
+    | 'close to deadline';
+  email: string;
+};
+
+const columns: ColumnDef<Nonprofit>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Name',
+  },
+  {
+    accessorKey: 'status',
+    header: 'Status',
+  },
+  {
+    accessorKey: 'email',
+    header: 'Email',
+    cell: ({ row }) => (
+      <a href={`mailto:${row.original.email}`} className="hover:underline">
+        {row.original.email}
+      </a>
+    ),
+  },
+];
+
+interface AdminDashboardTableProps {
+  data: Nonprofit[];
+}
+
+export function AdminDashboardTable({ data }: AdminDashboardTableProps) {
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
