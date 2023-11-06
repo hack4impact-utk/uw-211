fb8980e0e5c463f74b3667933774fca309983a12
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2023-11-06 13:18:41 -0500

    made signin form and page using example

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,67 @@
+import Link from 'next/link';
+import SignInForm from '@/components/SignInForm';
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-sky-800" />
+          <div className="relative z-20 flex items-center text-lg font-medium">
+            {/* Replace SVG with UW211 logo here... */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            United Way 211
+          </div>
+          <div className="relative z-20 mt-auto">
+            <blockquote className="space-y-2">
+              <p className="text-lg">
+                &ldquo;United Way of Greater Knoxville is driving change in our
+                community by uniting people, businesses, and organizations.
+                Together, we&apos;re creating a more equitable Knoxville where
+                we have stable housing, financial security, quality early care
+                and education, access to food, and more.{' '}
+                <b>Join us and answer the call.</b>&rdquo;
+              </p>
+            </blockquote>
+          </div>
+        </div>
+        <div className="p-8">
+          <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">Sign in</h1>
+            </div>
+            <SignInForm />
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{' '}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{' '}
+              and{' '}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/SignInForm/index.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { SyntheticEvent, useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+
+interface SignInFormProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface IconProps extends React.HTMLAttributes<SVGElement> {}
+
+const Icons = {
+  microsoft: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      height="24px"
+      width="24px"
+      {...props}
+    >
+      <path
+        d="M0 0h2311v2310H0zm2564 0h2311v2310H2564zM0 2564h2311v2311H0zm2564 0h2311v2311H2564"
+        fill="currentColor"
+        width="24"
+      />
+    </svg>
+  ),
+  spinner: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+    </svg>
+  ),
+};
+
+export default function SignInForm({ className, ...props }: SignInFormProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  async function onClick(event: SyntheticEvent) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }
+
+  return (
+    <div className={cn('grid gap-6', className)} {...props}>
+      <Button variant="outline" onClick={onClick} disabled={isLoading}>
+        {isLoading ? (
+          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+        ) : (
+          <Icons.microsoft className="mr-2 h-4 w-4" />
+        )}{' '}
+        Microsoft SSO
+      </Button>
+    </div>
+  );
+}
