ad4dc2800ebfa46b15c1aa7bb558887c06cc78f9
Jacob Hawkins <jacob.hawkins010@gmail.com>
2023-11-09 09:44:17 -0500

    moved formWrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import FormWrapper from '@/components/FormWrapper/FormWrapper';
-
 export default function Home() {
   return (
     <>
       <h1>Home page</h1>
-      <FormWrapper />
     </>
   );
 }
