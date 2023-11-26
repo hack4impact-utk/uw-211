bf4df531e1ca48e68a1504849dd50f2017b214d9
Rudra Patel <patelrudra2003@gmail.com>
2023-11-26 00:40:05 -0500

    Connect id query param to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,33 @@
-'use client';
-
 import WelcomePage from '@/components/WelcomePage';
-import { useSearchParams } from 'next/navigation';
+import { Agency } from '@/utils/types';
 
-export default function Home() {
-  const searchParams = useSearchParams();
-  const id = searchParams.get('id') ?? 'Participant';
+async function getAgency(id: string): Promise<Agency> {
+  const res = await fetch(`${process.env.BASE_URL}/api/agencies/${id}`);
+  const body = await res.json();
+  return body.data.agency;
+}
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  // extract id query parameter
+  let id = searchParams.id;
+
+  // if multiple id query parameters, just take the first one
+  if (typeof id === 'object') {
+    id = id[0];
+  }
+
+  let agency;
+  if (id) {
+    agency = await getAgency(id);
+  }
 
   return (
     <>
-      <WelcomePage id={id}></WelcomePage>
+      <WelcomePage id={id} agency={agency}></WelcomePage>
     </>
   );
 }
