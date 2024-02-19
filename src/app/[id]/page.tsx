0d90927ae6cbe68fe92a561d7282b21d33d49a2d
Jacob Hawkins <jacob.hawkins010@gmail.com>
2024-02-19 10:35:36 -0500

    days of operation mobile fix

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -200,7 +200,7 @@ export default function Form({ params }: { params: { id: string } }) {
                         id="akas"
                         {...register('akas')}
                         autoComplete="akas"
-                        className="block h-10 w-full rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600  sm:text-sm sm:leading-6"
+                        className="block h-10 w-full rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6  md:w-2/3 xl:w-full"
                       />
                       {errors.akas?.message && (
                         <p className="mt-2 text-sm text-red-400">
@@ -224,7 +224,7 @@ export default function Form({ params }: { params: { id: string } }) {
                         v-model="legalStatus"
                         {...register('legalStatus')}
                         autoComplete="legalStatus"
-                        className="block h-10 w-full rounded-md border-0 bg-inherit p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                        className="block h-10 w-full rounded-md border-0 bg-inherit p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6 md:w-2/3 xl:w-full"
                       >
                         <option value="">Please Select One</option>
                         <option value="federal">Federal</option>
@@ -246,7 +246,7 @@ export default function Form({ params }: { params: { id: string } }) {
                 </section>
 
                 {/* Hours of Operation */}
-                <div className="md:col-span-3 md:row-span-2">
+                <div>
                   <label
                     htmlFor="legalStatus"
                     className="block text-sm font-medium leading-6 text-gray-900"
@@ -254,179 +254,192 @@ export default function Form({ params }: { params: { id: string } }) {
                     Hours of Operation
                   </label>
                   <div className="mt-2">
-                    <fieldset className="mt-8">
+                    <fieldset>
                       <label
                         htmlFor="legalStatus"
-                        className="block text-sm font-medium leading-6 text-gray-900"
+                        className="mb-2 block text-sm font-medium leading-6 text-gray-900"
                       >
                         Select day(s) of operation
                       </label>
-                      <label>
-                        <input
-                          type="checkbox"
-                          id="monday"
-                          className="form-checkbox hidden"
-                          checked={isMondayChecked}
-                          {...register('days.monday', {
-                            onChange: () => {
-                              setMondayChecked(!isMondayChecked);
-                            },
-                          })}
-                        />
-                        <span
-                          className={
-                            isMondayChecked
-                              ? 'w-18 ml-2 rounded-sm bg-sky-600 px-2 py-1 text-white'
-                              : 'w-18 ml-2 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
-                          }
-                        >
-                          Monday
-                        </span>
-                      </label>
 
-                      <label>
-                        <input
-                          type="checkbox"
-                          id="tuesday"
-                          className="form-checkbox hidden"
-                          checked={isTuesdayChecked}
-                          {...register('days.tuesday', {
-                            onChange: () => {
-                              setTuesdayChecked(!isTuesdayChecked);
-                            },
-                          })}
-                        />
-                        <span
-                          className={
-                            isTuesdayChecked
-                              ? 'w-18 ml-2 rounded-sm bg-sky-600 px-2 py-1 text-white'
-                              : 'w-18 ml-2 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
-                          }
-                        >
-                          Tuesday
-                        </span>
-                      </label>
+                      <div className="flex flex-col gap-4 sm:flex-row sm:gap-2">
+                        <div className="flex flex-row gap-2">
+                          {/* Monday */}
+                          <label>
+                            <input
+                              type="checkbox"
+                              id="monday"
+                              className="form-checkbox hidden"
+                              checked={isMondayChecked}
+                              {...register('days.monday', {
+                                onChange: () => {
+                                  setMondayChecked(!isMondayChecked);
+                                },
+                              })}
+                            />
+                            <span
+                              className={
+                                isMondayChecked
+                                  ? 'w-18 rounded-sm bg-sky-600 px-2 py-1 text-white'
+                                  : 'w-18 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
+                              }
+                            >
+                              Monday
+                            </span>
+                          </label>
 
-                      <label>
-                        <input
-                          type="checkbox"
-                          id="wednesday"
-                          className="form-checkbox hidden"
-                          checked={isWednesdayChecked}
-                          {...register('days.wednesday', {
-                            onChange: () => {
-                              setWednesdayChecked(!isWednesdayChecked);
-                            },
-                          })}
-                        />
-                        <span
-                          className={
-                            isWednesdayChecked
-                              ? 'w-18 ml-2 rounded-sm bg-sky-600 px-2 py-1 text-white'
-                              : 'w-18 ml-2 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
-                          }
-                        >
-                          Wednesday
-                        </span>
-                      </label>
+                          {/* Tuesday */}
+                          <label>
+                            <input
+                              type="checkbox"
+                              id="tuesday"
+                              className="form-checkbox hidden"
+                              checked={isTuesdayChecked}
+                              {...register('days.tuesday', {
+                                onChange: () => {
+                                  setTuesdayChecked(!isTuesdayChecked);
+                                },
+                              })}
+                            />
+                            <span
+                              className={
+                                isTuesdayChecked
+                                  ? 'w-18 rounded-sm bg-sky-600 px-2 py-1 text-white'
+                                  : 'w-18 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
+                              }
+                            >
+                              Tuesday
+                            </span>
+                          </label>
 
-                      <label>
-                        <input
-                          type="checkbox"
-                          id="thursday"
-                          className="form-checkbox hidden"
-                          checked={isThursdayChecked}
-                          {...register('days.thursday', {
-                            onChange: () => {
-                              setThursdayChecked(!isThursdayChecked);
-                            },
-                          })}
-                        />
-                        <span
-                          className={
-                            isThursdayChecked
-                              ? 'w-18 ml-2 rounded-sm bg-sky-600 px-2 py-1 text-white'
-                              : 'w-18 ml-2 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
-                          }
-                        >
-                          Thursday
-                        </span>
-                      </label>
+                          {/* Wednesday */}
+                          <label>
+                            <input
+                              type="checkbox"
+                              id="wednesday"
+                              className="form-checkbox hidden"
+                              checked={isWednesdayChecked}
+                              {...register('days.wednesday', {
+                                onChange: () => {
+                                  setWednesdayChecked(!isWednesdayChecked);
+                                },
+                              })}
+                            />
+                            <span
+                              className={
+                                isWednesdayChecked
+                                  ? 'w-18 rounded-sm bg-sky-600 px-2 py-1 text-white'
+                                  : 'w-18 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
+                              }
+                            >
+                              Wednesday
+                            </span>
+                          </label>
+                        </div>
 
-                      <label>
-                        <input
-                          type="checkbox"
-                          className="form-checkbox hidden"
-                          id="friday"
-                          checked={isFridayChecked}
-                          {...register('days.friday', {
-                            onChange: () => {
-                              setFridayChecked(!isFridayChecked);
-                            },
-                          })}
-                        />
-                        <span
-                          className={
-                            isFridayChecked
-                              ? 'w-18 ml-2 rounded-sm bg-sky-600 px-2 py-1 text-white'
-                              : 'w-18 ml-2 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
-                          }
-                        >
-                          Friday
-                        </span>
-                      </label>
+                        <div className="flex flex-row gap-2">
+                          {/* Thursday */}
+                          <label>
+                            <input
+                              type="checkbox"
+                              id="thursday"
+                              className="form-checkbox hidden"
+                              checked={isThursdayChecked}
+                              {...register('days.thursday', {
+                                onChange: () => {
+                                  setThursdayChecked(!isThursdayChecked);
+                                },
+                              })}
+                            />
+                            <span
+                              className={
+                                isThursdayChecked
+                                  ? 'w-18 rounded-sm bg-sky-600 px-2 py-1 text-white'
+                                  : 'w-18 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
+                              }
+                            >
+                              Thursday
+                            </span>
+                          </label>
+
+                          {/* Friday */}
+                          <label>
+                            <input
+                              type="checkbox"
+                              className="form-checkbox hidden"
+                              id="friday"
+                              checked={isFridayChecked}
+                              {...register('days.friday', {
+                                onChange: () => {
+                                  setFridayChecked(!isFridayChecked);
+                                },
+                              })}
+                            />
+                            <span
+                              className={
+                                isFridayChecked
+                                  ? 'w-18 rounded-sm bg-sky-600 px-2 py-1 text-white'
+                                  : 'w-18 rounded-sm bg-slate-200 px-2 py-1  text-gray-900'
+                              }
+                            >
+                              Friday
+                            </span>
+                          </label>
+                        </div>
+                      </div>
                       {errors.days?.message && (
                         <p className="mt-2 text-sm text-red-400">
                           {errors.days.message}
                         </p>
                       )}
                     </fieldset>
+                  </div>
+                </div>
 
-                    <div className="float-left mt-6 flex justify-center gap-x-4">
-                      <div className="mt-1">
-                        <label
-                          htmlFor="open"
-                          className="block text-sm font-medium leading-6 text-gray-900"
-                        >
-                          Open
-                        </label>
-                        <input
-                          type="text"
-                          id="open"
-                          {...register('open')}
-                          autoComplete="open"
-                          className="block h-10 rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600  sm:text-sm sm:leading-6"
-                        />
-                        {errors.open?.message && (
-                          <p className="mt-2 text-sm text-red-400">
-                            {errors.open.message}
-                          </p>
-                        )}
-                      </div>
-                      <div className="flex items-end justify-center">
-                        <p>to</p>
-                      </div>
-                      <div className="mt-1">
-                        <label
-                          htmlFor="close"
-                          className="block text-sm font-medium leading-6 text-gray-900"
-                        >
-                          Close
-                        </label>
-                        <input
-                          type="text"
-                          id="close"
-                          {...register('close')}
-                          autoComplete="close"
-                          className="block h-10 rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600  sm:text-sm sm:leading-6"
-                        />
-                        {errors.close?.message && (
-                          <p className="mt-2 text-sm text-red-400">
-                            {errors.close.message}
-                          </p>
-                        )}
-                      </div>
-                    </div>
+                {/* Open/Close */}
+                <div className="float-left mt-6 flex justify-center gap-x-4">
+                  <div className="mt-1">
+                    <label
+                      htmlFor="open"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Open
+                    </label>
+                    <input
+                      type="text"
+                      id="open"
+                      {...register('open')}
+                      autoComplete="open"
+                      className="block h-10 rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600  sm:text-sm sm:leading-6"
+                    />
+                    {errors.open?.message && (
+                      <p className="mt-2 text-sm text-red-400">
+                        {errors.open.message}
+                      </p>
+                    )}
+                  </div>
+                  <div className="flex items-end justify-center">
+                    <p>to</p>
+                  </div>
+                  <div className="mt-1">
+                    <label
+                      htmlFor="close"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Close
+                    </label>
+                    <input
+                      type="text"
+                      id="close"
+                      {...register('close')}
+                      autoComplete="close"
+                      className="block h-10 rounded-md border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600  sm:text-sm sm:leading-6"
+                    />
+                    {errors.close?.message && (
+                      <p className="mt-2 text-sm text-red-400">
+                        {errors.close.message}
+                      </p>
+                    )}
                   </div>
                 </div>
               </section>
