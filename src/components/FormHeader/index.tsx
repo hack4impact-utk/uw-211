5f7ac36fed62f8653206c07e72eac6ac60b24cbf
Caleb Fisher <cfishe36@vols.utk.edu>
2024-02-20 01:55:04 -0500

    fixes header and form overlap

--- src/components/FormHeader/index.tsx
+++ src/components/FormHeader/index.tsx
@@ -4,7 +4,7 @@ import uw211Logo from '@/../public/img/unitedway211.png';
 
 export default function Header() {
   return (
-    <header className="fixed top-0 flex w-full border-b-2 border-[#1e57a1] bg-orange-500 pb-2 pt-2">
+    <header className="fixed top-0 z-50 flex w-full border-b-2 border-[#1e57a1] bg-orange-500 pb-2 pt-2">
       <div className="flex flex-1 items-center justify-center">
         <Image
           src={uw211Logo.src}
