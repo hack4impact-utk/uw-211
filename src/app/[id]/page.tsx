5689606725369a545128ffecc94294ef9b0e7a19
Caleb Fisher <cfishe36@vols.utk.edu>
2024-02-27 13:01:30 -0500

    small fix to conditional form logic

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -794,7 +794,7 @@ export default function Form({ params }: { params: { id: string } }) {
                           type="text"
                           {...register('don_coor')}
                           id="don_coor"
-                          disabled={pickupChecked === 'false'}
+                          disabled={donationChecked === 'false'}
                           className="h-8 w-full rounded-sm border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  focus:ring-sky-600 sm:text-sm sm:leading-6"
                         />
                         {errors.don_coor?.message && (
@@ -813,7 +813,7 @@ export default function Form({ params }: { params: { id: string } }) {
                           type="tel"
                           {...register('don_coor_tel')}
                           id="don_coor_tel"
-                          disabled={pickupChecked === 'false'}
+                          disabled={donationChecked === 'false'}
                           className="h-8 w-full rounded-sm border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  focus:ring-sky-600 sm:text-sm sm:leading-6"
                         />
                         {errors.don_coor_tel?.message && (
