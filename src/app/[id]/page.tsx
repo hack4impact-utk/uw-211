c8f50ebe91a5b949ce7632ad9274c33fbf2e55d3
Jacob Hawkins <jacob.hawkins010@gmail.com>
2024-03-19 14:00:53 -0400

    fix mobile spacing

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1974,7 +1974,7 @@ homeless men, etc.) This helps us to make appropriate referrals."
               </section>
 
               {/* Opportunities */}
-              <section className="flex flex-col gap-4">
+              <section className="mt-8 flex flex-col gap-4 sm:mt-0">
                 <h2 className="text-base font-semibold leading-7 text-gray-900">
                   Opportunities
                 </h2>
