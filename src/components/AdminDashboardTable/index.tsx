f723897155ed9169a92f09d2b475fef12858b544
Caleb Fisher <cfishe36@vols.utk.edu>
2024-03-26 01:10:39 -0400

    resolves property naming errors

--- src/components/AdminDashboardTable/index.tsx
+++ src/components/AdminDashboardTable/index.tsx
@@ -174,9 +174,9 @@ export async function AdminDashboardTable({
           >
             <path
               fill="currentColor"
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M10 6.5a3.5 3.5 0 1 1-7 0a3.5 3.5 0 0 1 7 0m-.691 3.516a4.5 4.5 0 1 1 .707-.707l2.838 2.837a.5.5 0 0 1-.708.708z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             />
           </svg>
         </button>
