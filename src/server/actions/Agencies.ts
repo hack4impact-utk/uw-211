25ecdfaf2f942d8787ac646e95dd83320d1ba193
Rudra Patel <patelrudra2003@gmail.com>
2024-04-07 17:11:19 -0400

    Add docs

--- src/server/actions/Agencies.ts
+++ src/server/actions/Agencies.ts
@@ -20,7 +20,7 @@ type CurrentStatusFilters = {
  * @brief Gets all agencies
  * @param populateServices Populates the agencies' "services" field
  * @param searchString Filters agencies that do not have the search term present in their name or
- * @param 
+ * @param currentStatusFilters Filters agencies based on their current status
  * @param compareFn Sorts the array of agencies before returning using the specified function; leave empty for no sorting
  * @returns An array of all agencies in the "agencies" collection
  */
