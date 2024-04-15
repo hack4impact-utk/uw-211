9a43afaee64e195862c11672de75e54abdcbd0c7
Jacob Hawkins <jacob.hawkins010@gmail.com>
2024-04-15 19:40:26 -0400

    typo fix

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -3154,7 +3154,7 @@ homeless men, etc.) This helps us to make appropriate referrals."
                             cols={30}
                             rows={10}
                             disabled={recommendationChecked === 'false'}
-                            placeholder="List type of volunteer work, age, traning, background checks, other requirements for your volunteers"
+                            placeholder="List type of volunteer work, age, training, background checks, other requirements for your volunteers"
                             className="mt-2 block h-28 w-full resize-none rounded-lg border-0 p-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  focus:ring-sky-600 sm:text-sm sm:leading-6"
                           ></textarea>
                           <div className="mt-2 min-h-6 ">
