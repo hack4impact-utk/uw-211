053ab3242af68e7a2471449eac4c1724c2b699dd
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2024-05-01 16:38:09 -0400

    added auth back to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,47 @@
-import createMiddleware from 'next-intl/middleware';
+import { withAuth } from 'next-auth/middleware';
+import { NextRequest } from 'next/server';
+import createIntlMiddleware from 'next-intl/middleware';
 import { locales, defaultLocale, localePrefix } from '@/i18n';
 
-export default createMiddleware({
+const privatePages = ['/dashboard'];
+
+const intlMiddleware = createIntlMiddleware({
   locales: locales,
   defaultLocale: defaultLocale,
   localePrefix: localePrefix,
 });
 
+const authMiddleware = withAuth(
+  function onSuccess(req) {
+    return intlMiddleware(req);
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => token != null,
+    },
+    pages: {
+      signIn: '/signin',
+    },
+  }
+);
+
+export default function middleware(req: NextRequest) {
+  const privatePathnameRegex = RegExp(
+    `^(/(${locales.join('|')}))?(${privatePages
+      .flatMap((p) => p)
+      .join('|')})/?$`,
+    'i'
+  );
+  const isPrivatePage = privatePathnameRegex.test(req.nextUrl.pathname);
+
+  if (isPrivatePage) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (authMiddleware as any)(req);
+  } else {
+    return intlMiddleware(req);
+  }
+}
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(en-US|es-US)/:path*'],
+  matcher: ['/((?!api|_next|.*\\..*).*)'],
 };
