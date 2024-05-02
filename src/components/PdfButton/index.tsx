056b4e641fa2cb2daf4a0fdab9fc08d3ff3fb123
Caleb Fisher <cfishe36@vols.utk.edu>
2024-05-01 21:06:47 -0400

    adds spinner to View Form button

--- src/components/PdfButton/index.tsx
+++ src/components/PdfButton/index.tsx
@@ -3,17 +3,22 @@
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Newspaper } from 'lucide-react';
+import { useState } from 'react';
+import Spinner from '@/components/Spinner';
 
 interface PdfButtonProps {
   agencyId: string | undefined;
 }
 
 const PdfButton: React.FC<PdfButtonProps> = ({ agencyId }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const downloadPdf = async (id: string | undefined) => {
     if (id === undefined) {
       return;
     }
 
+    setIsLoading(true);
     const pdfBytes = await fetch(`/api/pdf/${id}`).then((res) =>
       res.arrayBuffer()
     );
@@ -22,13 +27,27 @@ const PdfButton: React.FC<PdfButtonProps> = ({ agencyId }) => {
       type: 'application/pdf;charset=utf-8',
     });
     const url = URL.createObjectURL(blob);
+    setIsLoading(false);
     window.open(url);
   };
 
   return (
-    <Button variant="outline" onClick={() => downloadPdf(agencyId)}>
-      <Newspaper className="mr-2" />
-      View form
+    <Button
+      variant="outline"
+      className="h-10 w-36"
+      onClick={() => downloadPdf(agencyId)}
+      disabled={isLoading}
+    >
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <Spinner className="h-4 w-4 animate-spin" />
+        </div>
+      ) : (
+        <div className="flex">
+          <Newspaper className="mr-2" />
+          View form
+        </div>
+      )}
     </Button>
   );
 };
